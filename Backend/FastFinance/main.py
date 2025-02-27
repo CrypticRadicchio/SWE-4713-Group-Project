f2047dd3a2eb1ad9eebb38d28eb589fcc89bd06e
Vaughn <bergervaughn@gmail.com>
2025-02-27 04:22:42 -0500

    Big Backend Update

--- Backend/FastFinance/main.py
+++ Backend/FastFinance/main.py
@@ -1,29 +1,29 @@
+from calendar import month
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
-from userinfo import User, Role, Status
+from datetime import date, timedelta
+import DummyDB
+import FFEmail
+from userinfo import User, Role, NewUserRequest, Email
+from fastapi.middleware.cors import CORSMiddleware
+from DummyDB import user_table, new_user_table
 
 app = FastAPI()
 
-db: List[User] = [
-    User(
-        id="testAdmin",
-        hashed_pass="temp",
-        # email is default for now so this test user can receive emails
-        role = Role.admin,
-        status=True,
-        first_name="Dave",
-        last_name="Administrator"
-    ),
-    User(
-        id="sampleAccountant",
-        hashed_pass="temp",
-        role=Role.accountant,
-        status=True,
-        first_name="John",
-        last_name="Accounting"
-    )
-]
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# adding a comment here
 
 class Request(BaseModel):
     type: str = None
@@ -31,24 +31,126 @@ class Request(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"Greeting": "Hello team. I have successfully hosted my API through ngrok. I am on a roll tonight."}
+    return {"Greeting": "You have accessed the root of the FastFinance API."}
 
+#the primary way the app will get user data to display on the screen
 @app.get("/users")
 async def fetch_users():
-    return db
+    return user_table
 
+@app.get("users/new_user")
+async def get_new_user_requests():
+    """
+    Returns the list of new user requests.
+    :return:
+    """
+    return {"message": "Unfinished function"}
+
+@app.get("/users/login")
+async def login(login_info: User):
+    """
+    This takes a special type of user with only 2 variables. the JSON format looks like:
+    {"id": "user id string", "hashed_password": "hashed password string"}
+
+    it will return a JSON with one field: "message", which includes the role of the user and " Login Successful", eg:
+    {"message": "Accountant Login Successful"}
+    {"message": "Manager Login Successful"}
+    {"message": "Admin Login Successful"}
+
+    depending on the user's role.
+
+    :param login_info:
+    :return:
+    """
+
+    DummyDB.check_outdated_passwords()
+    # the above line is to check every current password in the system if they are about to expire, and send an email if so.
+    # this happens during every login request because at least one user logging in is a very frequent and consistent action
+    #
+
+    user = DummyDB.get_user(login_info.id) #raises an exception if user id not found
+
+    if user.hashed_pass == login_info.hashed_pass:
+        if not user.status:
+            return {"Error": "Account Suspended"}
+        if user.role == Role.admin:
+            return {"message": "Admin Login Successful"}
+        elif user.role == Role.manager:
+            return {"message": "Accountant Login Successful"}
+        else:
+            return {"message": "Accountant Login Successful"}
+    else:
+        user.failed_attempts+=1
+        return {"Error": "Incorrect Password"}
+
+@app.get("/users/'login/forgot_password")
+async def forgot_pass(login_info: User):
+    """
+    This also takes a special user JSON, this time with just their ID and their email in the following format:
+    {"id": <user id string>, "email": <email string>}
+
+    if successful, it will return the user's security questions in the following json:
+    {
+        "security_question_1": <security question 1 string>,
+        "security_question_2": <security question 2 string>,
+        "security_question_3": <security question 3 string>,
+        "security_answer_1": <security answer 1 string>,
+        "security_answer_2": <security answer 2 string>,
+        "security_answer_3": <security answer 3 string>,
+    }
+    It is expected that the web client will handle the logic. Once the answers have been verified, the client use the update_users()
+    function defined by the path: /users/update with the "put" parameter
+    :param login_info:
+    :return:
+    """
+    user = DummyDB.get_user(login_info.id)
+
+    return {"security_question": user.security_question, "security_answer": user.security_answer}
+
+# The primary way the admin will add a user to the system.
 @app.post("/users")
 async def register_user(user: User):
-    db.append(user)
+    user_table.append(user)
     return {"id": user.id}
 #weird shit going on tonight
 
+@app.post("users/new_user")
+async def new_user(user: NewUserRequest):
+    """
+    Function to create a "new user" request in the system.
+    The format for a new user request JSON looks like:
+    {
+        "first_name": <first name string>
+        "last_name": <last name string>
+        "email": <email address string>
+        "dob": <string in format dd/mm/yyyy>
+    }
+    Admins will see new user requests on their client.
+    To create a new user in the database, use the post:"/users" api call.
+
+    :param user:
+    :return:
+    """
+    new_user_table.append(user)
+    return {"email": user.email}
+
+@app.post("/email")
+async def send_email(email: Email):
+    result = FFEmail.send_email(email.recipient, email.subject, email.body)
+    return {"Message": result}
+
+# the primary way an admin will update user info.
+# this includes changing personal info about the user and activating or deactivating them
 @app.put("/users/update")
 async def update_user(user: User):
-    for u in db:
+    for u in user_table:
         if user.id == u.id:
             if user.hashed_pass is not None:
+                for old_pass in u.past_passwords:
+                    if user.hashed_pass == old_pass:
+                        raise HTTPException(405, {"Error": "New Password cannot be an old password."})
                 u.hashed_pass = user.hashed_pass
+                u.password_expiration = date.today() + timedelta(days=90)
             if user.email is not None:
                 u.email = user.email
             if user.status is not None:
@@ -62,3 +164,12 @@ async def update_user(user: User):
         status_code=404,
         detail=f"User with ID: {user.id} does not exist."
     )
+
+@app.delete("/users/new_user")
+async def delete_new_user_request(email: str):
+    for user in new_user_table:
+        if email == user:
+            new_user_table.remove(user)
+            return {"Message": "New User Request successfully deleted."}
+
+    raise HTTPException(404, "User not found.")
--- Backend/FastFinance/userinfo.py
+++ Backend/FastFinance/userinfo.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from pydantic import BaseModel
 from enum import Enum
+from typing import List
 
 class Role(str, Enum):
     admin = "admin"
@@ -13,9 +16,32 @@ class Status(str, Enum):
 class User(BaseModel):
     id: str
     hashed_pass: str
+    past_passwords: List[str] = []
     email: str = "bergervaughn@gmail.com" # default is my email
     role: Role
     status: bool = True
     first_name: str
     last_name: str
+    dob: str = "01-01-1900"
+    failed_attempts: int = 0
+    password_expiration: str = "01-05-2025"
+    security_question: str = "What is your mother's maiden name?"
+    security_answer: str = "Jones"
 
+class NewUserRequest(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    dob: str
+
+class Email(BaseModel):
+    recipient: str
+    subject: str
+    body: str
+
+
+# class PastPasswords(BaseModel):
+#     pass_id: str
+#     user_id: str
+#     hashed_pass: str
+#     date_created: date
